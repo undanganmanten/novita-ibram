3ec6d41c18a90766e15ea2228631e558203b57ec
undanganmanten <agungtprass@gmail.com>
2022-05-12 14:18:51 +0700

    novita ibram

--- js/script.js
+++ js/script.js
@@ -81,23 +81,23 @@ var x = setInterval(function() {
 
 
 //kirim hadiah
-let modal = document.querySelector(".modal");
-let trigger = document.querySelector(".trigger");
-let closeButton = document.querySelector(".close-button");
+// let modal = document.querySelector(".modal");
+// let trigger = document.querySelector(".trigger");
+// let closeButton = document.querySelector(".close-button");
 
-function toggleModal() {
-    modal.classList.toggle("show-modal");
-}
+// function toggleModal() {
+//     modal.classList.toggle("show-modal");
+// }
 
-function windowOnClick(event) {
-    if (event.target === modal) {
-        toggleModal();
-    }
-}
+// function windowOnClick(event) {
+//     if (event.target === modal) {
+//         toggleModal();
+//     }
+// }
 
-trigger.addEventListener("click", toggleModal);
-closeButton.addEventListener("click", toggleModal);
-window.addEventListener("click", windowOnClick);
+// trigger.addEventListener("click", toggleModal);
+// closeButton.addEventListener("click", toggleModal);
+// window.addEventListener("click", windowOnClick);
 
 
 //firebase
